eeee1f6618b8c725fedb6c66357aec18cf828713
aparna0003 <aparna0003@.github.com>
2020-09-18 03:56:23 +0530

    components

---
+++ pages/oneColumnWidth.vue
@@ -0,0 +1,182 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-row
+      class="mb-6"
+      no-gutters
+    >
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 6 : undefined"
+        <breadcrumb/>
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          {{ n }} of 3 {{ n === 1 ? '(wider)' : '' }}
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <carousel/>
+      >
+       <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <card/>
+      >
+       <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <toolbar/>
+      >
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <avatar/>
+      >
+       <v-col
+        v-for="n in 2"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <breadcrumb/>
+      >
+       <v-col
+        v-for="n in 2"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <hover/>
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          {{ n }} of 3 {{ n === 2 ? '(wider)' : '' }}
+        </v-card>
+      </v-col>
+    </v-row>
+     <v-row
+      class="mb-6"
+      no-gutters
+    >
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 6 : undefined"
+        <breadcrumb/>
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          {{ n }} of 3 {{ n === 1 ? '(wider)' : '' }}
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <loader/>
+      >
+       <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <tab/>
+      >
+       <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <button/>
+      >
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <breadcrumb/>
+      >
+       <v-col
+        v-for="n in 2"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <breadcrumb/>
+      >
+      >
+       <v-col
+        v-for="n in 2"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <slideGroup/>
+         <v-col
+        v-for="n in 2"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <icon/>
+      >
+        <v-card
+          class="pa-3"
+          outlined
+          tile
+        >
+          {{ n }} of 3 {{ n === 2 ? '(wider)' : '' }}
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row
+      class="mb-6"
+      no-gutters
+    >
+     
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          {{ n }} of 3 {{ n === 2 ? '(wider)' : '' }}
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 1"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <loader/>
+      >    
+      <v-col
+        v-for="n in 1"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <form/>
+      >  
+       <v-col
+        v-for="n in 1"
+        :key="n"
+        :cols="n === 2 ? 5 : undefined"
+        <slideGroup/>
+      >  
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          {{ n }} of 3 {{ n === 1 ? '(wider)' : '' }}
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ pages/playground.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-row
+          :align="alignment"
+          :justify="justify"
+          class="grey lighten-5"
+          style="height: 300px;"
+        >
+          <v-card
+            v-for="n in 3"
+            :key="n"
+            class="ma-3 pa-6"
+            outlined
+            tile
+          >
+            Column hiiiiii. Shopclues here
+            <icon/>
+          </v-card>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row justify="center">
+           
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-select
+              v-model="alignment"
+              :items="alignmentsAvailable"
+              label="Align"
+              <carousel/>
+            ></v-select>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="2"
+          >
+            <v-select
+              v-model="justify"
+              :items="justifyAvailable"
+              label="Justify"
+              <card/>
+            ></v-select>
+             <v-select
+              v-model="justify"
+              :items="justifyAvailable"
+              label="Justify"
+              <card/>
+            ></v-select>
+             <v-select
+              v-model="justify"
+              :items="justifyAvailable"
+              label="Justify"
+              <toolbar/>
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        alignmentsAvailable: [
+          'start',
+          'center',
+          'end',
+          'baseline',
+          'stretch',
+        ],
+        alignment: 'center',
+        dense: false,
+        justifyAvailable: [
+          'start',
+          'center',
+          'end',
+          'space-around',
+          'space-between',
+        ],
+        justify: 'center',
+      }
+    },
+  }
+</script>
