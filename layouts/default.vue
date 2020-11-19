bbf8c217efa998c6d1f8aaffe93ebfecf619b164
Saksham-12 <60287258+Saksham-12@users.noreply.github.com>
2020-11-19 19:59:45 +0530

    adding cart page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,158 +1,161 @@
+<!-- This is the main default layout page. -->
 <template>
-  <!--Your App Starts Here -->
-  <v-app id="inspire" light>
-    
-    <!--Header -->
-    <!--<v-app-bar color="red" dark fixed app>
-      <v-toolbar-title>Application</v-toolbar-title>-->
-       <!-- Component Example -->
-       <v-row
-         
-          no-gutters
-          col="12"
-         
-         >
-          <v-col
-           
-           class=" col-xs-1 col-sm-1 col-md-2 col-lg-3 light"
-          >
-          <v-card
-             
-              class="cyan lighten-5 "
-               pa="2" 
-               tile
-               max-height="500"
-               elevation= "0 px"
-               >
-              <VuetifyLogo/>  <!--persistant tool bar is added-->
-          </v-card>
-         </v-col>
-        
-         <v-col
-           class="  col-xs-4 col-sm-7 col-md-9 col-lg-9 light"
-           v-offset="2"
-          ><toolbar/>  <!--persistant tool bar is added-->
-         </v-col>
-         </v-row>
-      <catagory/>     <!--catagory bar for all pages is-->
-    </v-app-bar>
-
-    <!-- Body -->
-    <v-content light>
-      <nuxt /> 
-    </v-content>
-          
-    <!-- Footer -->
-    <v-footer color="white" 
-    light> <!--colour of the footer is changed-->
-    <!--footer content is added-->
-     <v-container class="menubar2">  <!--first div for the conversion of the footer-->
-      <v-row class="smallbody ">
-    <!--adding the colums one by one-->
-      <v-col >
-      <h5>Shopper Central</h5>
-     <p> Easy Returns & Replacement </p>
-     <p> Our Policies</p>
-     <p> CluesBucks</p>
-     <p> Payment Options</p>
-     <p> ShopClues Surety</p>
-      <p> FAQs (Help) </p>
-      <p> Service Centers </p>
-      <p> User Agreement </p>
-      <p> Refer & Earn </p>
-      </v-col>
-       
-      <v-col>
-      <h5>Merchant Central </h5>
-      <p> Merchant Panel </p>
-      <p> How To Sell </p>
-      <p> Commission Structure </p>
-      <p> Fullfillment Policy </p>
-      <p> Policies & Rules </p>
-      <p>User Agreement </p>
-      <p>Testimonials </p>
-      <p>Seller Summit</p>
-      <p> Merchant Blog </p>
-      <p> Developer Resources </p>
-      </v-col>
-
-
-      <v-col>
-      <h5>About Us</h5>
-       <p>History </p>
-       <p> Band of Trust </p>
-       <p> Brand Guidelines </p>
-      <p> TV Commercials </p>
-      <p> In the News </p>
-      <p> Awards </p>
-      <p> Careers </p>
-      <p> Blog </p>
-      </v-col>
-
-
-      <v-col>
-        <h5>Contact Us</h5>
-         <p> Merchant Support </p>
-         <p> Bulk Orders </p>
-         <p> Press </p>
-      </v-col>
-      </v-row >
-      <v-row>
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-      </v-row>
-      </v-container>
-
-       <v-container class="menubar1"> <!--second div for the conversion of the footer-->
-      <v-row>
-
-      <v-col >
-     
-      <h5 class="code">Useful Links</h5>
-     <p class="smallbody"> Easy Returns & Replacements  |  Privacy Policy  |  User Agreement  |
-         Notes to Security Practices  |  Labor Compliance  |  Shopper Central  |
-         Merchant Central  |  About Us  |  Contact Us  |  Press 
-      </p>
-     
- </v-col>
-       
-      
-
-
-      
-
-      </v-row >
-      <v-row>
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-      </v-row>
-      </v-container>
-    </v-footer>
-
-  </v-app>
+   <!--Your App Starts Here -->
+   <v-app>
+      <!-- This is the header. -->
+      <v-card class="br-0">
+         <v-toolbar class="background-ACE0E5">
+            <!-- This is the company logo. -->
+            <v-img max-width="140px" src="logo1.png" class="removeBoxShadow" elevation-0></v-img>
+            <v-spacer></v-spacer>
+            <v-card class="hidden-xs-only removeBoxShadow br-0" style="background:white; display: inherit;width: 50% ;height:60% ;">
+               <input type="text" class="pl-2" color="black" style="width:100%; outline:none;" placeholder="Explore The Beyond"></input>
+               <!-- This is the search icon. -->
+               <v-icon color="#00A4B4" class="mr-2 cursorPointer">mdi-magnify</v-icon>
+            </v-card>
+            <v-spacer></v-spacer>
+            <!-- These are the list of icons. -->
+            <v-icon color="#00A4B4" class="pa-1 cursorPointer">mdi-map-marker-outline</v-icon>
+            <v-icon color="#00A4B4" class="pa-1 cursorPointer">mdi-bell-outline</v-icon>
+            <v-icon color="#00A4B4" class="pa-1 cursorPointer">mdi-heart-outline</v-icon>
+            <v-icon color="#00A4B4" class="pa-1 cursorPointer">mdi-cart-outline</v-icon>
+            <!-- The user account details have to be loaded from a clickable dropdown in case of sm and smaller screens.
+               So class="hidden-sm-and-down" is added is to hide for sm and smaller screens. 
+               It will be visible for all screens md and larger than md. 
+               This menu lists down the user specific drop down items.
+               -->          
+            <v-menu class="background-ACE0E5">
+               <template v-slot:activator="{ on, dropdownMenu }">
+                  <!-- ma-0 and pa-0 are classes to add margin and padding respectively. 
+                     Other classes are defined in style tag to render specific styles.-->
+                  <v-btn class="ma-0 pa-1 hidden-sm-and-down background-ACE0E5 cursorPointer color-black removeBoxShadow"                     
+                     v-bind="dropdownMenu"
+                     v-on="on"
+                     >
+                     Hi ABC 
+                     <v-icon class="pa-0" color="#00A4B4">mdi-menu-down</v-icon>
+                  </v-btn>
+               </template>
+               <v-list class="background-ACE0E5 color-black">
+                  <v-list-item
+                     v-for="item in userProdileDropdown"
+                     :key="item.title"
+                     >
+                     <v-list-item-title class="color-black">{{ item.title }}</v-list-item-title>
+                  </v-list-item>
+               </v-list>
+            </v-menu>
+            <!-- This menu bar is to display  the title only for sm and xs screens 
+               class="hidden-md-and-up" helps us to hide for all screens md and larger than md. -->
+            <v-menu class="ma-0 pa-0 background-ACE0E5">
+               <template v-slot:activator="{ on, attrs }">
+                  <v-btn class="hidden-md-and-up ma-0 pa-0" 
+                     icon
+                     v-bind="attrs"
+                     v-on="on"
+                     >
+                     <v-icon color="#00A4B4" >mdi-dots-vertical</v-icon>
+                  </v-btn>
+               </template>
+               <!-- The user account details which were hidden for sm and small screens are being populated here for those screens only. -->
+               <v-menu class="background-ACE0E5">
+                  <template v-slot:activator="{ on, dropdownMenu }">
+                     <v-btn class="background-ACE0E5 color-black removeBoxShadow" style="width: 100%; text-align:left"                     
+                        v-bind="dropdownMenu"
+                        v-on="on"
+                        >
+                        Hi ABC 
+                        <v-icon class="pa-0" color="#00A4B4">mdi-menu-down</v-icon>
+                     </v-btn>
+                  </template>
+                  <v-list class="background-ACE0E5 color-black">
+                     <v-list-item
+                        v-for="item in userProdileDropdown"
+                        :key="item.title"
+                        >
+                        <v-list-item-title class="color-black">{{ item.title }}</v-list-item-title>
+                     </v-list-item>
+                  </v-list>
+               </v-menu>
+               <v-list class="background-ACE0E5 color-black">
+                  <v-list-item
+                     v-for="item in menu"
+                     :key="item.title"
+                     >
+                     <v-list-item-title class="color-black">{{ item.title }}</v-list-item-title>
+                  </v-list-item>
+               </v-list>
+            </v-menu>
+         </v-toolbar>
+         <!-- Search box which is to be displayed only in case of sm and xs screens. -->
+         <v-card class="hidden-sm-and-up removeBoxShadow br-0" style="background:white;color:#00A4B4; display: inherit;">
+            <input type="text" color="black" class="pa-0 pl-4" style="width:85%; outline:none" placeholder="Explore The Beyond"></input>
+            <!-- This is the search icon. -->
+            <v-icon class="ma-2" color="#00A4B4">mdi-magnify</v-icon>
+         </v-card>
+      </v-card>
+      <!-- This menu bar is to display  the title only for md and above screens.
+         class="hidden-sm-and-down" hides it for all sm and smaller screens. 
+         The code to display menu bar on sm screen is already added above. -->
+      <v-card tile class="hidden-sm-and-down br-0">
+         <v-toolbar dense color="#00A4B4">
+            <v-toolbar-items>
+               <v-btn class="removeBoxShadow" style="font-size:12px" color="#00A4B4"
+                  v-for="item in menu"
+                  :key="item.title"
+                  >{{ item.title }}</v-btn>
+            </v-toolbar-items>
+         </v-toolbar>
+      </v-card>
+      <v-card class="pa-2 br-0" style="background:#D3EAEC; height:100%;">
+         <CouponPopup/>
+         <Product/>
+      </v-card>
+   </v-app>
 </template>
-
+<!-- This is the script to include title list in header.. 
+   This can then be rendered / displayed using a loop and thus making an object like this makes it easy to display details. -->
 <script>
-export default {}
+   export default {
+     data: () => ({
+       menu: [
+         { title: 'Made in India' },
+         { title: 'Men' },
+         { title: 'Women' },
+         { title: 'Home & Furniture' },
+         { title: 'Electronics' },
+         { title: 'Appliances' },
+         { title: 'Sports & More' },
+         { title: 'Daily Essentials' } 
+       ],
+      userProdileDropdown: [
+         { title: 'My Orders' },
+         { title: 'My Returns' },
+         { title: 'My Profile' },
+         { title: 'My Chat' },
+         { title: 'My Feedback' },
+         { title: 'My Cluesbucks' },
+         { title: 'Help and Support' },
+         { title: 'Sign Out' } 
+       ],
+     }),
+   }
 </script>
-
-<style> 
-  .black--text {
-    border: 1px solid;
-  }
-
-   #inspire {
-    background-color:white ;
-    color: black;
-
-  }
-  .menubar2 {
-           display:none;
-         }
-  @media (min-width: 1025px) {
-         .menubar1 {
-           display:none;
-         }
-         .menubar2 {
-           display:block;
-         }
-      }
+<!-- Script tag contains the definitions od user defined styling -->
+<style>
+   .background-ACE0E5{
+   background : #ACE0E5 !important
+   }
+   .color-black{
+   color : black !important
+   }
+   .cursorPointer {
+   cursor : pointer;
+   }
+   .removeBoxShadow{
+   box-shadow: 0 0 !important
+   }
+   .br-0{
+   border-radius: 0px !important
+   }
 </style>
